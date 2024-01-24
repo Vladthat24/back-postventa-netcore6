08453a97b67babafd2b34d6bd0876dc6a4fc1029
ycondorisoluciones <ycondori.sdigitales>
2024-01-24 18:55:49 -0500

    Agregando comentario

--- POS.Aplication/Comnons/Orderning/PaginateQuery.cs
+++ POS.Aplication/Comnons/Orderning/PaginateQuery.cs
@@ -4,6 +4,7 @@ namespace POS.Aplication.Comnons.Orderning
 {
     public static class PaginateQuery
     {
+        //Paginacion
         public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, BasePaginationRequest request)
         {
             return queryable.Skip((request.NumPage - 1) * request.Records).Take(request.Records);
